2ee3fce9a315df9398e21fd0557d04e0d3ee7e33
MartinsRoberto <robertomartinsramm@gmail.com>
2022-12-06 23:52:46 -0300

    login

---
+++ Login/script.js
@@ -0,0 +1,15 @@
+// const form = document.querySelector('.form')
+
+// form.addEventListener('click', (e) => {
+//   if(e.target.classList[0] == 'input'){
+//     const label = e.target.parentNode.querySelector('label').innerText
+//     const letters = label.split('')
+//     console.log(letters)
+
+//     letters.forEach((letter, i) => {
+//       setTimeout(() => {
+//         letter.style.top = '-20px'
+//       }, 1*i)
+//     });
+//   }
+// })
